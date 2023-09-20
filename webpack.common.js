00ae0e57e9447d6d6085f0e5a37ea4ba2852f072
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-09-20 15:24:17 +0800

    build

--- webpack.common.js
+++ webpack.common.js
@@ -31,7 +31,11 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/inline',
-            }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ]
     }
 }
